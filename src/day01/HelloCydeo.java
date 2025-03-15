0c53caab843c556dda93c9bcc599da4b8ee3b457
drighmounir <drigh.mounir@gmail.com>
2025-03-15 18:11:52 -0400

    Update HelloCydeo.java

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -2,7 +2,11 @@ package day01;
 
 public class HelloCydeo {
     public static void main(String[] args) {
+        System.out.println("hello world");
         System.out.println("hello cydeo");
+        System.out.println("hello programmers");
+
+          
     }
 
 }
